962f0050f60457ab12312cba9108dc000cffc917
Sorebit <cmbk90@gmail.com>
2017-03-19 21:35:36 +0100

    Better buffer setters

--- server/buffer.js
+++ server/buffer.js
@@ -1,6 +1,8 @@
 // ArrayBuffer wrapper for more elegant byte setting
 // Setters set values in order they are passed
 
+var Util = require('./util');
+
 function Buffer(size) {
 	this._ab = new ArrayBuffer(size);
 	this._dv = new DataView(this._ab);
@@ -11,54 +13,50 @@ function Buffer(size) {
 	this.e = false;
 }
 
+Buffer.prototype._set = function(thisArg, size, args) {
+	if(!thisArg || !size || !args) {
+		Util.error('Invalid setter');
+		return;
+	}
+	for(var i = 0; i < args.length; ++i) {
+		thisArg.call(this._dv, this.o + size * i, args[i], this.e);
+	}
+	this.o += size * args.length;
+}
+
 Buffer.prototype.setInt8 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setInt8(this.o + i, arguments[i], this.e);
-	this.o += 1 * arguments.length;
+	this._set(DataView.prototype.setInt8, 1, arguments);
 }
 
 Buffer.prototype.setUint8 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setUint8(this.o + i, arguments[i], this.e);
-	this.o += 1 * arguments.length;
+	this._set(DataView.prototype.setUint8, 1, arguments);
 }
 
 Buffer.prototype.setInt16 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setInt16(this.o + i * 2, arguments[i], this.e);
-	this.o += 2 * arguments.length;
+	this._set(DataView.prototype.setInt16, 2, arguments);
 }
 
 Buffer.prototype.setUint16 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setUint16(this.o + i * 2, arguments[i], this.e);
-	this.o += 2 * arguments.length;
+	this._set(DataView.prototype.setUint16, 2, arguments);
 }
 
 Buffer.prototype.setInt32 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setInt32(this.o + i * 4, arguments[i], this.e);
-	this.o += 4 * arguments.length;
+	this._set(DataView.prototype.setInt32, 4, arguments);
 }
 
 Buffer.prototype.setUint32 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setUint32(this.o + i * 4, arguments[i], this.e);
-	this.o += 4 * arguments.length;
+	this._set(DataView.prototype.setUint32, 4, arguments);
 }
 
 Buffer.prototype.setFloat32 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setFloat32(this.o + i * 4, arguments[i], this.e);
-	this.o += 4 * arguments.length;
+	this._set(DataView.prototype.setFloat32, 4, arguments);
 }
 
 Buffer.prototype.setFloat64 = function() {
-	for(var i = 0; i < arguments.length; ++i)
-		this._dv.setFloat64(this.o + i * 8, arguments[i], this.e);
-	this.o += 8 * arguments.length;
+	this._set(DataView.prototype.setFloat64, 8, arguments);
 }
 
+// Add string terminator?
 Buffer.prototype.setString16 = function(str) {
 	for(var i = 0; i < str.length; i++) {
 		this._dv.setUint16(this.o + i * 2, str.charCodeAt(i), this.e);
